ea6c9c07615174ab4ec77c3b497d0a9779c2a996
Maria Mudrakova <mmudrako@my.centennialcollege.ca>
2025-01-08 20:37:49 -0800

    04_03_completed

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -49,12 +49,14 @@ function Main({ dishes }) {
   );
 }
 function App() {
-  const [status, setStatus] = useState("Open");
-  console.log(status);
+  const [status, setStatus] = useState(true);
+
   return (
     <div>
-      <h1>The restaurant is currently {status}</h1>
-      <button onClick={() => setStatus("Closed")}>Close Restaurant</button>
+      <h1>The restaurant is currently {" "}{status ? "open" : "closed"}.</h1>
+      <button onClick={() => setStatus(!status)}>
+        {status ? "Close" : "Open"} Restaurant
+      </button>
       <Header name="Alex" year={new Date().getFullYear()} />
       <Main dishes={dishObjects} />
     </div>
