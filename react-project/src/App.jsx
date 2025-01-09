65a87b9a39e06519e9e155845b3b56a56451c255
Maria Mudrakova <mmudrako@my.centennialcollege.ca>
2025-01-09 09:47:44 -0800

    04_04_completed

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -22,11 +22,14 @@ const dishObjects = items.map((dish, i) => ({
   title: dish
 }));
 
-function Main({ dishes }) {
+function Main({ dishes, openStatus, onStatus }) {
   return (
     <>
       <div>
-        <h2>Welcome to this beautiful restaurant!</h2>
+        <button onClick={() => onStatus(true)}>I want to be open</button>
+        <h2>Welcome to this beautiful restaurant! {" "}
+          {openStatus ? "Open" : "Closed"}
+        </h2>
       </div>
       <main>
         <img
@@ -53,12 +56,15 @@ function App() {
 
   return (
     <div>
-      <h1>The restaurant is currently {" "}{status ? "open" : "closed"}.</h1>
+      <h1>
+        The restaurant is currently{" "}
+        {status ? "open" : "closed"}.
+      </h1>
       <button onClick={() => setStatus(!status)}>
         {status ? "Close" : "Open"} Restaurant
       </button>
       <Header name="Alex" year={new Date().getFullYear()} />
-      <Main dishes={dishObjects} />
+      <Main dishes={dishObjects} openStatus={status} onStatus={setStatus}/>
     </div>
   );
 }
